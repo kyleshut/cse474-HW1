86429085f4c80bc43658e6c0167dda6615b162ff
kyleshut <54827927+kyleshut@users.noreply.github.com>
2020-10-06 11:48:18 -0400

    Update script.py

--- basecode/script.py
+++ basecode/script.py
@@ -188,7 +188,7 @@ def regressionObjVal(w, X, y, lambd):
     y = y.flatten()
     error =  .5 * (np.sum((y-np.dot(w, np.transpose(X))) * (y - np.dot(X,w))) + lambd * np.dot(np.transpose(w),w))
     error_grad = np.dot(np.dot(np.transpose(X), X), w) - np.dot(np.transpose(X), y) + lambd * w
-    error_grad = error_grad.flatten()
+	error_grad = error_grad.flatten()
 
     return error, error_grad
 
